ccecb81ecb742129c27e74652dd246e4fa77753d
cxwww <510880341@qq.com>
2017-07-04 17:36:42 +0800

    c

--- demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
