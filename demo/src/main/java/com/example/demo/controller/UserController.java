8650bde83c7eb9b95427da7499fc87175ed0f5aa
cxwww <510880341@qq.com>
2017-07-05 10:34:21 +0800

    d

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +9,16 @@ import com.example.demo.dao.UserRepository;
 import com.example.demo.domain.User;
 
 @RestController
-@EnableAutoConfiguration
 public class UserController {
 
 	@Autowired
-	UserRepository ur;
+	UserRepository userRepository;
 	
 	@RequestMapping("/getUser")
 	@Cacheable(value="user_cache")
 	public User getUser() {
-		System.out.println("1");
-		User user = ur.findByName("a1");
+		System.out.println(userRepository);
+		User user = userRepository.findByName("a1");
 		System.out.println("无缓存效果");
 		return user;
 	}
